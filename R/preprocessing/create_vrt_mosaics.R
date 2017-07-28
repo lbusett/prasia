d68a1a962ef5dc3315b907023f0d5245161635cf
lbusett <lbusett@gmail.com>
2017-07-28 15:26:44 +0200

    final create oredered mosaics

--- R/preprocessing/create_vrt_mosaics.R
+++ R/preprocessing/create_vrt_mosaics.R
@@ -18,6 +18,8 @@ create_vrt_mosaics <- function(mosaics_folder, pattern) {
 library(tidyverse)
 library(gdalUtils)
 library(foreach)
+library(parallel)
+library(doSNOW)
 mosaics_folder <- "/home/lbusetto/nas-s4a/nr_working/shared/PhenoRice/Asia/Data/mosaics/by_year/"
 patterns <- c("sos", "eos", "pos", "cumevi", "veglgt", "totlgt", "nseas")
 
@@ -30,17 +32,41 @@ vrts    <- list.files(mosaics_folder, pattern = ".vrt", full.names = T)
 outfold <- "/home/lbusetto/nas-s4a/nr_working/shared/PhenoRice/Asia/Data/mosaics/ordered"
 dir.create(outfold)
 
+ncores <- 8
+clust <- parallel::makeCluster(ncores, outfile = "")
+doSNOW::registerDoSNOW(clust)
+
 out <- foreach(vrt_n = seq_len(vrts),
                .combine      = "c",
                .verbose      = FALSE,
-               .packages     = c("gdalUtils") %dopar% {
+               .packages     = c("gdalUtils"),
+               .verbose      = TRUE) %dopar% {
 
+                 message("create_vrt_tiff --> processing: ", basename(vrts[vrt_n]))
+                 parameter <- basename(tools::file_path_sans_ext(vrts[vrt_n]))
                  outfile <- file.path(outfold, paste0(basename(tools::file_path_sans_ext(vrts[vrt_n])), ".tif"))
-                 gdalUtils::gdalbuildvrt(vrts[vrt_n], outfile, separate = TRUE)
-                 return (paste0(outfile, " completed ! "))
-               }
-)
+                 if (parameter %in% c("sos_ordered", "eos_ordered", "pos_ordered")) {
+                   ot <- "Int16"
+                 }
+                 if (parameter %in% c("cumevi_ordered")) {
+                   ot <- "Int32"
+                 }
+                 if (parameter %in% c("totlgt_ordered", "veglgt_ordered", "nseas_ordered")) {
+                   ot <- "Byte"
+                 }
 
+                 gdalUtils::gdal_translate(vrts[vrt_n],
+                                           outfile,
+                                           ot        = "Int16",
+                                           of        = "GTiff",
+                                           co        = "COMPRESS=DEFLATE",
+                                           separate  = TRUE,
+                                           verbose   = TRUE,
+                                           overwrite = TRUE)
+
+                 return(paste0(outfile, " completed ! "))
+               }
+parallel::stopCluster(clust)
 
 
 
